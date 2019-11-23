868e5c1a67622f66d6c51199e295b3320e600c3f
monpro <monproshen@gmail.com>
2019-11-23 16:59:09 +1100

    init reducers'

--- src/index/reducers.js
+++ src/index/reducers.js
@@ -1,10 +1,92 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import {
+  SET_IS_DATE_SELECTOR_VISIBLE,
+  SET_IS_CITY_SELECTOR_VISIBLE,
+  SET_CURRENT_SELECTING_LEFT_CITY,
+  SET_HIGH_SPEED,
+  SET_IS_LOADING_CITY_DATE,
+  SET_CITY_DATE,
+  SET_FROM,
+  SET_TO
+} from "./actions";
 
-import reducers from "./reducers";
-import thunk from "redux-thunk";
+export default {
+  from(state = "Sydney", action) {
+    const { type, payload } = action;
+    switch (type) {
+      case SET_FROM:
+        return payload;
+      default:
+    }
+    return state;
+  },
 
-export default createStore(
-  combineReducers(reducers),
-  {},
-  applyMiddleware(thunk)
-);
+  to(state = "Melbourne", action) {
+    const { type, payload } = action;
+    switch (type) {
+      case SET_TO:
+        return payload;
+      default:
+    }
+    return state;
+  },
+
+  isCitySelectorVisible(state = false, action) {
+    const { type, payload } = action;
+    switch (type) {
+      case SET_IS_CITY_SELECTOR_VISIBLE:
+        return payload;
+      default:
+    }
+    return state;
+  },
+
+  currentSelectingLeftCity(state = false, action) {
+    const { type, payload } = action;
+    switch (type) {
+      case SET_CURRENT_SELECTING_LEFT_CITY:
+        return payload;
+      default:
+    }
+    return state;
+  },
+
+  cityDate(state = null, action) {
+    const { type, payload } = action;
+    switch (type) {
+      case SET_CITY_DATE:
+        return payload;
+      default:
+    }
+    return state;
+  },
+
+  isLoadingCityDate(state = false, action) {
+    const { type, payload } = action;
+    switch (type) {
+      case SET_IS_LOADING_CITY_DATE:
+        return payload;
+      default:
+    }
+    return state;
+  },
+
+  isDateSelectorVisible(state = false, action) {
+    const { type, payload } = action;
+    switch (type) {
+      case SET_IS_DATE_SELECTOR_VISIBLE:
+        return payload;
+      default:
+    }
+    return state;
+  },
+
+  highSpeed(state = false, action) {
+    const { type, payload } = action;
+    switch (type) {
+      case SET_HIGH_SPEED:
+        return payload;
+      default:
+    }
+    return state;
+  }
+};
