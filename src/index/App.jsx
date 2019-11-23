1501c3a0694833aad8f24273511edcd43f6914d2
monpro <monproshen@gmail.com>
2019-11-23 17:00:33 +1100

    init app.jsx

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -1,6 +1,23 @@
-import "./App.css";
+import React from "react";
 import { connect } from "react-redux";
-const App = props => {};
+import "./App.css";
+import Header from "../common/Header";
+import DepartDate from "./DepartDate";
+import HighSpeed from "./HighSpeed";
+import Submit from "./Submit";
+import Journey from "./Journey";
+
+const App = props => {
+  return (
+      <div>
+          <Header />
+          <Journey />
+          <DepartDate />
+          <HighSpeed />
+          <Submit />
+      </div>
+  );
+};
 
 export default connect(
   null,
