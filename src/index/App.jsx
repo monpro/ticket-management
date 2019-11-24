a1ec29ccc65d9d79f5e2f06d59a96fe43ef4c1f8
monpro <monproshen@gmail.com>
2019-11-24 15:58:42 +1100

    useCallBack to reduce rerender

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { connect } from "react-redux";
 import "./App.css";
 import Header from "../common/Header";
@@ -8,14 +8,19 @@ import Submit from "./Submit";
 import Journey from "./Journey";
 
 const App = props => {
+  const onBack = useCallback(() => {
+    window.history.back();
+  }, []);
   return (
-      <div>
-          <Header />
-          <Journey />
-          <DepartDate />
-          <HighSpeed />
-          <Submit />
+    <div>
+      <div className="header-wrapper">
+        <Header title="Tickets" onBack={onBack} />
       </div>
+      <Journey />
+      <DepartDate />
+      <HighSpeed />
+      <Submit />
+    </div>
   );
 };
 
