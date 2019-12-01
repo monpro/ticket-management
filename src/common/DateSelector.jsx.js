9b2eec00444339fe0e626639fe579942c3083d56
monpro <monproshen@gmail.com>
2019-12-01 19:06:54 +1100

    get 3 months nearby

--- src/common/DateSelector.jsx.js
+++ src/common/DateSelector.jsx.js
@@ -6,13 +6,37 @@ import Header from "./Header";
 
 import "./DateSelector.css";
 
+const Month = props => {
+  const { startingTimeInMonth } = props;
+
+  return <div></div>;
+};
+
 export default function DateSelector(props) {
   const { show, onSelect, onBack } = props;
+  const now = new Date();
+  now.setHours(0);
+  now.setSeconds(0);
+  now.setMilliseconds(0);
+
+  now.setDate(1);
+
+  const monthSequence = [now.getTime()];
+
+  now.setMonth(now.getMonth() + 1);
+  monthSequence.push(now.getTime());
+
+  now.setMonth(now.getMonth() + 1);
+  monthSequence.push(now.getTime());
 
   return (
     <div className={classnames("date-selector", { hidden: !show })}>
       <Header onBack={onBack} title={"Select Date"} />
-      <div className="date-selector-tables"></div>
+      <div className="date-selector-tables">
+        {monthSequence.map(month => {
+          return <Month key={month} startingTimeInMonth={month} />;
+        })}
+      </div>
     </div>
   );
 }
