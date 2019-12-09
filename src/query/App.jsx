a56923f9ef1d2fad0b336966639cb8070e6d5d8e
monpro <monproshen@gmail.com>
2019-12-09 21:51:44 +1100

    add Header and basic settings

--- src/query/App.jsx
+++ src/query/App.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useCallback } from "react";
 import "./App.css";
 import { connect } from "react-redux";
-
+import Header from "../common/Header";
 import Nav from "../common/Nav";
 import List from "./List";
 import Filter from "./Filter";
+
 const App = props => {
+  const { from, to } = props;
+
+  const onBack = useCallback(() => {
+    window.history.back();
+  }, []);
+
   return (
     <div>
+      <div className="header-wrapper">
+        <Header onBack={onBack} title={`${from} - ${to}`} />
+      </div>
       <Nav />
       <List />
       <Filter />
