c8781f716c5d075470828a31c5a26d32abcbeebf
monpro <monproshen@gmail.com>
2019-12-09 22:30:47 +1100

    generate url useEffect

--- src/query/App.jsx
+++ src/query/App.jsx
@@ -9,10 +9,33 @@ import Nav from "../common/Nav";
 import List from "./List";
 import Filter from "./Filter";
 
-import { setFrom, setTo, setDepartDate, setHighSpeed } from "./actions";
+import {
+  setFrom,
+  setTo,
+  setDepartDate,
+  setHighSpeed,
+  setSearchParsed
+} from "./actions";
 
 const App = props => {
-  const { from, to, dispatch } = props;
+  const {
+    from,
+    to,
+    dispatch,
+    searchParsed,
+    departDate,
+    highSpeed,
+    orderType,
+    onlyTickets,
+    checkedTicketTypes,
+    checkedTrainTypes,
+    checkedDepartStations,
+    checkedArriveStations,
+    departTimeStart,
+    departTimeEnd,
+    arriveTimeStart,
+    arriveTimeEnd
+  } = props;
 
   const onBack = useCallback(() => {
     window.history.back();
@@ -26,8 +49,50 @@ const App = props => {
     dispatch(setTo(to));
     dispatch(setDepartDate(getDateWithDay(dayjs(date).valueOf())));
     dispatch(setHighSpeed(highSpeed === "true"));
+    dispatch(setSearchParsed(true));
   }, []);
 
+  useEffect(() => {
+    if (!searchParsed) {
+      return;
+    }
+    const url = new URI("/rest/query")
+      .setSearch("from", from)
+      .setSearch("to", to)
+      .setSearch("date", dayjs(departDate).format("YYYY-MM-DD"))
+      .setSearch("orderType", orderType)
+      .setSearch("onlyTickets", onlyTickets)
+      .setSearch("checkedTicketTypes", Object.keys(checkedTicketTypes).join())
+      .setSearch("checkedTrainTypes", Object.keys(checkedTrainTypes).join())
+      .setSearch(
+        "checkedDepartStations",
+        Object.keys(checkedDepartStations).join()
+      )
+      .setSearch(
+        "checkedArriveStations",
+        Object.keys(checkedArriveStations).join()
+      )
+      .setSearch("departTimeStart", departTimeStart)
+      .setSearch("departTimeEnd", departTimeEnd)
+      .setSearch("arriveTimeStart", arriveTimeStart)
+      .setSearch("arriveTimeEnd", arriveTimeEnd)
+      .toString();
+  }, [
+    from,
+    to,
+    departDate,
+    highSpeed,
+    orderType,
+    onlyTickets,
+    checkedTicketTypes,
+    checkedTrainTypes,
+    checkedDepartStations,
+    checkedArriveStations,
+    departTimeStart,
+    departTimeEnd,
+    arriveTimeStart,
+    arriveTimeEnd
+  ]);
   return (
     <div>
       <div className="header-wrapper">
