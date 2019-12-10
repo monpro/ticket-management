67e560723d5a54f1ba9042a852de020537e6c3c4
monpro <monproshen@gmail.com>
2019-12-10 22:56:51 +1100

    set up Nav component

--- src/common/Nav.jsx
+++ src/common/Nav.jsx
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useMemo, memo } from "react";
 import PropTypes from "prop-types";
+import classnames from "classnames";
+import dayjs from "dayjs";
 import "./Nav.css";
 
-export default function Nav(props) {
-  return <div></div>;
-}
+const Nav = memo(function(props) {
+  const { date, prev, next, isPrevDisabled, isNextDisabled } = props;
 
-Nav.propTypes = {};
+  const currentDateString = useMemo(() => {
+    const dateObj = dayjs(date);
+    return dateObj.format("M.D") + dateObj.format("ddd");
+  }, [date]);
+  return (
+    <div className="nav">
+      <span
+        onClick={prev}
+        className={classnames("nav-prev", {
+          "nav-disabled": isPrevDisabled
+        })}
+      >
+        prev Day
+      </span>
+      <span className="nav-current">{currentDateString}</span>
+      <span
+        onClick={next}
+        className={classnames("nav-prev", {
+          "nav-disabled": isNextDisabled
+        })}
+      >
+        Next Day
+      </span>
+    </div>
+  );
+});
+
+export default Nav;
+
+Nav.propTypes = {
+  date: PropTypes.number.isRequired,
+  prev: PropTypes.func.isRequired,
+  next: PropTypes.func.isRequired,
+  isPrevDisabled: PropTypes.bool.isRequired,
+  isNextDisabled: PropTypes.bool.isRequired
+};
