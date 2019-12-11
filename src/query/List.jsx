106ddd14fc9d76416f7c2d4da5c291dc30a9bf62
monpro <monproshen@gmail.com>
2019-12-11 20:48:53 +1100

    add List component and style

--- src/query/List.jsx
+++ src/query/List.jsx
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { memo } from "react";
 import PropTypes from "prop-types";
 import "./List.css";
 
-export default function List(props) {
-  return <div></div>;
-}
+const ListItem = memo(props => {
+  const {
+    dTime,
+    aTime,
+    dStation,
+    aStation,
+    trainNumber,
+    date,
+    time,
+    priceMsg,
+    dayAfter
+  } = props;
 
-List.propTypes = {};
+  return <li className="list-item"></li>;
+});
+
+ListItem.propTypes = {
+  dTime: PropTypes.string.isRequired,
+  aTime: PropTypes.string.isRequired,
+  dStation: PropTypes.string.isRequired,
+  aStation: PropTypes.string.isRequired,
+  trainNumber: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  time: PropTypes.string.isRequired,
+  priceMsg: PropTypes.string.isRequired,
+  dayAfter: PropTypes.string.isRequired
+};
+
+const List = memo(props => {
+  const { list } = props;
+
+  return (
+    <ul className="list">
+      {list.map(item => (
+        <ListItem {...item} key={item.trainNumber} />
+      ))}
+    </ul>
+  );
+});
+
+List.propTypes = {
+  list: PropTypes.array.isRequired
+};
+
+export default List;
