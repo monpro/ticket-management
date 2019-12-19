9c77d2aba5f4363879d77e83c3e3b526c05d03ba
monpro <monproshen@gmail.com>
2019-12-20 09:56:50 +1100

    init App.jsx

--- src/ticket/App.jsx
+++ src/ticket/App.jsx
@@ -1,8 +1,37 @@
 import "./App.css";
+import React from "react";
 import { connect } from "react-redux";
-const App = props => {};
+const App = props => {
+  const {
+    departDate,
+    arriveDate,
+    departTimeStr,
+    arriveTimeStr,
+    departStation,
+    arriveStation,
+    trainNumber,
+    durationStr,
+    tickets,
+    isScheduleVisible,
+    searchParsed,
 
+    dispatch
+  } = props;
+
+  return <div className="app"></div>;
+};
+
+const mapStateToProps = state => state;
+
+const mapDispatchToProps = dispatch => {
+  return {
+    dispatch
+  };
+};
 export default connect(
   null,
-  {}
+  {
+    mapStateToProps,
+    mapDispatchToProps
+  }
 )(App);
