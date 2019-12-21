539a40b8354e7b9ae8eb7ee277664a9205a27856
monpro <monproshen@gmail.com>
2019-12-21 20:15:48 +1100

    add url parser

--- src/ticket/App.jsx
+++ src/ticket/App.jsx
@@ -1,10 +1,22 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect } from "react";
+import URI from "urijs";
 import { connect } from "react-redux";
 
 import Detail from "../common/Detail.jsx";
 import Candidate from "./Candidate";
 import Schedule from "./Schedule";
+
+import {
+  setDepartStation,
+  setArriveStation,
+  setTrainNumber,
+  setDepartdate,
+  setSearchParsed
+} from "./actions";
+import dayjs from "dayjs";
+import { getDateWithDay } from "../common/helper";
+
 const App = props => {
   const {
     departDate,
@@ -22,6 +34,17 @@ const App = props => {
     dispatch
   } = props;
 
+  useEffect(() => {
+    const queries = URI.parseQuery(window.location.search);
+    const [aStation, dStation, trainNumber, date] = queries;
+
+    dispatch(setArriveStation(aStation));
+    dispatch(setDepartStation(dStation));
+    dispatch(setTrainNumber(trainNumber));
+    dispatch(setDepartdate(getDateWithDay(dayjs(date).valueOf())));
+
+    dispatch(searchParsed(true));
+  }, []);
   return <div className="app"></div>;
 };
 
