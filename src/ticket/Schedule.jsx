ee4631a92e478532b5ae2059b9d07a4e50adf280
monpro <monproshen@gmail.com>
2019-12-24 12:28:02 +1100

    use memo to optimize the rendering of Schedule component

--- src/ticket/Schedule.jsx
+++ src/ticket/Schedule.jsx
@@ -1,9 +1,46 @@
-import React from "react";
-import propTypes from "prop-types";
+import React, { memo } from "react";
+import PropTypes from "prop-types";
+import classnames from "classnames";
 import "./Schedule.css";
 
-export function Schedule(props) {
-  return <div></div>;
-}
+const ScheduleRow = memo(props => {
+  const {
+    index,
+    station,
+    arriveTime,
+    departTiem,
+    stay,
 
-Schedule.propTypes = {};
+    isStartStation,
+    isEndStation,
+    isDepartStation,
+    isArriveStation,
+    beforeDepartStation,
+    afterArriveStation
+  } = props;
+
+  return (
+    <div>
+      <div
+        className={classnames("icon", {
+          "icon-red": isDepartStation || isArriveStation
+        })}
+      ></div>
+    </div>
+  );
+});
+
+const Schedule = memo(function Schedule(props) {
+  const { departDate, trainNumber, departStation, arriveStation } = props;
+
+  return <div className="schedule">schedule</div>;
+});
+
+Schedule.propTypes = {
+  departDate: PropTypes.string.isRequired,
+  trainNumber: PropTypes.string.isRequired,
+  departStation: PropTypes.string.isRequired,
+  arriveStation: PropTypes.string.isRequired
+};
+
+export default Schedule;
