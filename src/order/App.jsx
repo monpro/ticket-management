dd5226a9279de52ad22888155f985721006778cf
monpro <monproshen@gmail.com>
2019-12-27 18:39:11 +1100

    create components and associate them with App

--- src/order/App.jsx
+++ src/order/App.jsx
@@ -1,8 +1,42 @@
+import React from "react";
 import "./App.css";
 import { connect } from "react-redux";
-const App = props => {};
+import Header from "../common/Header";
+import Account from "./Account";
+import Choose from "./Choose";
+import Passengers from "./Passengers";
+import Ticket from "./Ticket";
 
+const App = props => {
+  const {
+    trainNumber,
+    departStation,
+    arriveStation,
+    seatType,
+    departDate,
+    arriveDate,
+    departTimeStr,
+    arriveTimeStr,
+    durationStr,
+    price,
+    passengers,
+    menu,
+    isMenuVisible,
+    searchParsed,
+    dispatch
+  } = props;
+
+  return <div className="app"></div>;
+};
+
+const mapStateToProps = state => state;
+
+const mapDispatchToProps = dispatch => {
+  return {
+    dispatch
+  };
+};
 export default connect(
-  null,
-  {}
+  mapStateToProps,
+  mapDispatchToProps
 )(App);
